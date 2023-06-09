17b9d6c67e1aa72db05ea1924d4fdf7135498f00
Thomas Lokshall <thomas.lokshall@gmail.com>
2023-06-09 09:50:14 +0200

    feat: use attribute names according to experimental semconv for graphql

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	github.com/99designs/gqlgen v0.17.32
+	github.com/vektah/gqlparser/v2 v2.5.2-0.20230422221642-25e09f9d292d
 	go.opentelemetry.io/otel v1.16.0
 	go.opentelemetry.io/otel/trace v1.16.0
 )
--- tracer.go
+++ tracer.go
@@ -5,22 +5,20 @@ import (
 	"fmt"
 
 	"github.com/99designs/gqlgen/graphql"
+	"github.com/vektah/gqlparser/v2/ast"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
+	semconv "go.opentelemetry.io/otel/semconv/v1.20.0"
 	"go.opentelemetry.io/otel/trace"
 )
 
 const (
 	extensionName          = "github.com/zhevron/gqlgen-opentelemetry"
-	graphqlComponent       = "graphql"
 	graphqlFieldName       = "graphql.field.name"
 	graphqlFieldPath       = "graphql.field.path"
 	graphqlFieldType       = "graphql.field.type"
-	graphqlOperationName   = "graphql.operation.name"
-	graphqlOperationType   = "graphql.operation.type"
 	graphqlVariablesPrefix = "graphql.variables."
-	graphqlQuery           = "graphql.query"
 )
 
 type Tracer struct {
@@ -39,11 +37,12 @@ func (t Tracer) Validate(schema graphql.ExecutableSchema) error {
 func (t Tracer) InterceptOperation(ctx context.Context, next graphql.OperationHandler) graphql.ResponseHandler {
 	oc := graphql.GetOperationContext(ctx)
 	operationName := getOperationName(oc)
-	ctx, span := t.tracer(ctx).Start(ctx, operationName, trace.WithAttributes(
-		attribute.String("component", graphqlComponent),
-		attribute.String(graphqlOperationName, operationName),
-		attribute.String(graphqlOperationType, string(oc.Operation.Operation)),
-		attribute.String(graphqlQuery, oc.RawQuery),
+	operationType := getOperationTypeAttribute(oc)
+	spanName := makeSpanName(operationName, operationType.Value.AsString())
+	ctx, span := t.getTracer(ctx).Start(ctx, spanName, trace.WithAttributes(
+		semconv.GraphqlOperationName(operationName),
+		operationType,
+		semconv.GraphqlDocument(oc.RawQuery),
 	))
 	if t.IncludeVariables {
 		for name, value := range oc.Variables {
@@ -63,7 +62,7 @@ func (t Tracer) InterceptField(ctx context.Context, next graphql.Resolver) (inte
 	if !fc.IsMethod || !fc.IsResolver {
 		return next(ctx)
 	}
-	ctx, span := t.tracer(ctx).Start(ctx, fc.Field.ObjectDefinition.Name+"."+fc.Field.Name, trace.WithAttributes(
+	ctx, span := t.getTracer(ctx).Start(ctx, fc.Field.ObjectDefinition.Name+"."+fc.Field.Name, trace.WithAttributes(
 		attribute.String(graphqlFieldName, fc.Field.Name),
 		attribute.String(graphqlFieldPath, fc.Path().String()),
 		attribute.String(graphqlFieldType, fc.Field.ObjectDefinition.Name),
@@ -79,7 +78,7 @@ func (t Tracer) InterceptField(ctx context.Context, next graphql.Resolver) (inte
 	return res, err
 }
 
-func (t Tracer) tracer(ctx context.Context) trace.Tracer {
+func (t Tracer) getTracer(ctx context.Context) trace.Tracer {
 	if t.Tracer != nil {
 		return t.Tracer
 	}
@@ -90,11 +89,35 @@ func (t Tracer) tracer(ctx context.Context) trace.Tracer {
 	}
 }
 
+func makeSpanName(operationName, operationType string) string {
+	spanName := operationType
+	if spanName == "" {
+		return "GraphQL Operation"
+	}
+	if operationName != "" {
+		spanName += " " + operationName
+	}
+	return spanName
+}
+
 func getOperationName(oc *graphql.OperationContext) string {
 	if oc.Operation.Name != "" {
 		return oc.Operation.Name
 	}
 	return oc.RawQuery
+}
+
+func getOperationTypeAttribute(oc *graphql.OperationContext) attribute.KeyValue {
+	switch oc.Operation.Operation {
+	case ast.Mutation:
+		return semconv.GraphqlOperationTypeMutation
+	case ast.Subscription:
+		return semconv.GraphqlOperationTypeSubscription
+	case ast.Query:
+		return semconv.GraphqlOperationTypeQuery
+	default:
+		return semconv.GraphqlOperationTypeQuery
+	}
 }
 
 func makeAttributeValue(value interface{}) attribute.Value {
@@ -116,7 +139,7 @@ func makeAttributeValue(value interface{}) attribute.Value {
 	case []interface{}:
 		return makeAttributeSliceValue(v)
 	default:
-		return attribute.StringValue(fmt.Sprintf("%v", v))
+		return attribute.StringValue(fmt.Sprintf("%+v", v))
 	}
 }
 
@@ -170,7 +193,7 @@ func makeAttributeSliceValue(value []interface{}) attribute.Value {
 	default:
 		arr := make([]string, len(value))
 		for i, v := range value {
-			arr[i] = fmt.Sprintf("%v", v)
+			arr[i] = fmt.Sprintf("%+v", v)
 		}
 		return attribute.StringSliceValue(arr)
 	}
